b60ddbc34e47740e97117883ecd00313f8a85d0c
Aleksandra Gabka <aleksandraxgabka@gmail.com>
2025-03-25 20:17:53 +0100

    Add Hotjar tracking script

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,29 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if each slide shows correct title and description', function () {
+    const expectedSlides = [
+      { title: 'Rome', description: 'Italy' },
+      { title: 'London', description: 'United Kingdom' },
+      { title: 'Paris', description: 'France' }
+    ];
+
+    cy.visit('http://localhost:3000');
+
+    expectedSlides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(1000);
+      }
+      cy.get('.swiper-slide-active .card-description h1')
+        .should('have.text', slide.title);
+
+      cy.get('.swiper-slide-active .card-description p')
+        .should('have.text', slide.description);
+    });
+  });
+});
+
